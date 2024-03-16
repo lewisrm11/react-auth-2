84d990315420cf7aa9eec78eca2ff860956cb7c7
carlos levi ricalde may <leviricalde@gmail.com>
2024-03-16 04:10:59 -0500

    second version: login and register

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,25 +1,44 @@
+// LoginPage.js
 import React, { useState } from 'react';
+import axios from 'axios';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [user, setUser] = useLocalStorage('user', null);
+  const [token, setToken] = useLocalStorage('token', null);
 
-  const handleLogin = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Implement login logic here (e.g., call API for authentication)
-    console.log('Login attempted with email:', email, 'password:', password);
-    setEmail('');
-    setPassword('');
+
+    try {
+      const response = await axios.post('http://localhost:8002/api/login', {
+        email,
+        password,
+      });
+
+      // Store the user in local storage
+      setUser(response.data);
+      // Store the user in local storage
+      setToken(response.data.data.token);
+
+      // Handle successful login here
+      console.log(response.data, user, token);
+    } catch (error) {
+      // Handle error here
+      console.error(error);
+    }
   };
 
   return (
     <div>
       <h2>Login</h2>
-      <form onSubmit={handleLogin}>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" required />
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' required />
         <button type="submit">Login</button>
       </form>
     </div>
--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -1,11 +1,38 @@
 // RegisterPage.js
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const RegisterPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8002/api/register', {
+        email: username,
+        password,
+        perfil: "test",
+        name: "test"
+      });
+
+      // Handle successful registration here
+      console.log(response.data);
+    } catch (error) {
+      // Handle error here
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>Register Page</h1>
-      {/* Add your register form here */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 };
